380d1ce6f10c6f8bddbc95d6f37bed34a5d78b4d
alexsharich <alexsharich@gmail.com>
2022-10-29 09:59:24 +0200

    add restAPI post/auth/login

--- index.js
+++ index.js
@@ -17,6 +17,43 @@ const app = express()
 
 app.use(express.json())// получение информации из тела запроса
 
+app.post('/auth/login', async (req,res)=>{
+  try{
+const user = await UserModel.findOne({email:req.body.email})
+if(!user){
+  return res.status(404).json({
+    message: 'User not found ...'
+  })
+}
+
+const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash) //проверка совпадают ли пароли
+if (!isValidPass){
+  return res.status(400).json({
+    message:'Invalid username or password ...'
+  })
+}
+
+const token = jwt.sign({
+  _id:user._id
+},'secret123',
+{
+  expiresIn:'30d'
+}) // создаем токен из id
+
+const {passwordHash,...userData} = user._doc;
+
+res.json({
+  ...userData,
+  token,
+}) // возвращаем информацию о пользователе и токен
+  }catch(err){
+console.log(err)
+res.status(500).json({
+  message:'Failed to auth...'
+})
+  }
+})
+
 app.post('/auth/register',registerValidation, async (req,res)=>{
 try{
   const errors = validationResult(req)
@@ -26,13 +63,13 @@ if(!errors.isEmpty()){
 
 const password = req.body.password
 const salt = await bcrypt.genSalt(10) // создаем соль для шифровки
-const passwordHash = await bcrypt.hash(password,salt) // создаем зашифрованный пароль
+const hash = await bcrypt.hash(password,salt) // создаем зашифрованный пароль
 
 const doc = new UserModel({
   email:req.body.email,
   fullName:req.body.fullName,
   avatarUrl:req.body.avatarUrl,
-  passwordHash
+  passwordHash:hash
 }) // создание пользователя для записи в базу данных
 
 const user = await doc.save() // создаем пользователя
@@ -44,14 +81,16 @@ const token = jwt.sign({
   expiresIn:'30d'
 }) // создаем токен из id
 
+const {passwordHash,...userData} = user._doc;
+
 res.json({
-  ...user,
+  ...userData,
   token
 }) // возвращаем информацию о пользователе и токен
 }catch(err){
   console.log(err)
 res.status(500).json({
-  message:'failed to register...'
+  message:'Failed to register...'
 })
 }
 })
